a03d157c4d7b890cd179b15b956220a6cc2ab9b1
nazarPtyt <ockap90@libero.it>
2022-10-28 14:49:49 +0200

    navBar modified

--- src/Components/navBar/NavBar.tsx
+++ src/Components/navBar/NavBar.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import CottageIcon from '@mui/icons-material/Cottage';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import SearchIcon from '@mui/icons-material/Search';
 
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import MailOutlineRoundedIcon from '@mui/icons-material/MailOutlineRounded';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import {Link} from "react-router-dom";
+import profilePhoto from '../../pages/register/register-bg.jpg'
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <div>
+            <div className='left'>
+                <Link to='/home'  className='logo'>
+                    <span>My Social</span>
+                </Link>
+                <CottageIcon/>
+                <DarkModeOutlinedIcon/>
+                <WidgetsIcon/>
+                <SearchIcon/>
+                <input type="text" placeholder={'Search...'}/>
+            </div>
+            <div className='right'>
+                <Person2OutlinedIcon/>
+                <MailOutlineRoundedIcon/>
+                <NotificationsActiveOutlinedIcon/>
+                {/*<img src={profilePhoto} alt="profile photo"/>*/}
+                <span>Nazar Prytuliak</span>
+            </div>
+        </div>
     );
 };
 
